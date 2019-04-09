b3e8deceee06f87d638faa70790ae7343c18f452
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2019-04-09 21:20:07 +0200

    Add some content and style to servives section

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+/* OWL open and config*/
+
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
 $('.owl-carousel').owlCarousel({
     items: 1
 })
+
+/* TAB SERVICES */
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
