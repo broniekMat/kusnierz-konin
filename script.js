5dcfd6b5a6d7b8bae6ea15dad514ec4092c52aaa
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2019-04-15 22:00:42 +0200

    Style price section

--- script.js
+++ script.js
@@ -24,4 +24,28 @@ function openServices(evt, service) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
+
+/* TAB PRICES */
+
+function openPrice(evt, price) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent-price");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink-price");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(price).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+function autoClick() {
+    document.querySelector(".defaultOpen-service").click();
+    document.querySelector(".defaultOpen-price").click();
+}
+
+autoClick();
